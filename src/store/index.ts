53bbaf07db41eea72242eb6112b920f9b5b05893
Dusan Morhac <55763425+dudo50@users.noreply.github.com>
2022-07-28 09:16:04 +0200

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,10 +5,14 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    account: "none"
   },
   getters: {
   },
   mutations: {
+    saveAccount(state, account){
+        state.account = account
+    }
   },
   actions: {
   },
